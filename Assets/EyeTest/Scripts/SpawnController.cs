43bbece2912eca71de934a635379e6242de4475d
sheaduarte <33766275+sheaduarte@users.noreply.github.com>
2020-05-14 19:42:57 -0700

    worked on spawn controller script (not yet done)

--- Assets/EyeTest/Scripts/SpawnController.cs
+++ Assets/EyeTest/Scripts/SpawnController.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     public GameObject spawnObject;
+    public Vector3[] spawnPosList;
+    private int spawnTotal;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
+        Vector3[] spawnPosList = new Vector3[9];
 
+        spawnPosList[0] = new Vector3 (0, 2, -5);
+        spawnPosList[1] = new Vector3 (0, 2, -7);
+        spawnPosList[2] = new Vector3 (0, 2, -9);
+        spawnPosList[3] = new Vector3 (3, 2, -5);
+        spawnPosList[4] = new Vector3 (3, 2, -7);
+        spawnPosList[5] = new Vector3 (3, 2, -9);
+        spawnPosList[6] = new Vector3 (6, 2, -5);
+        spawnPosList[7] = new Vector3 (6, 2, -7);
+        spawnPosList[8] = new Vector3 (6, 2, -9);
+
+
+    }
     // Update is called once per frame
-    void Update()
+    void SpawnInNewPos()
     {
-        
+        foreach(Vector3 item in spawnPosList)
+        {
+            Instantiate(spawnObject, item);
+        }
     }
 }
 
