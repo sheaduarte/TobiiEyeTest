833a67b5a97ffc1586813d68dbe55715d24ff138
sheaduarte <33766275+sheaduarte@users.noreply.github.com>
2020-06-30 12:14:33 -0700

    spawn controller update

--- Assets/EyeTest/Scripts/SpawnController.cs
+++ Assets/EyeTest/Scripts/SpawnController.cs
@@ -5,43 +5,76 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     public GameObject spawnObject;
-    public Vector3[] spawnPosList;
-    private int spawnTotal;
+    public Transform[] spawnPosList;
+    public float spawnTime;
+    public float stimulusDuration;
+    private bool keyPressed = false;
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        spawnPosList = new Vector3[9];
-
-        spawnPosList[0] = new Vector3 (0, 2, -5);
-        spawnPosList[1] = new Vector3 (0, 2, -7);
-        spawnPosList[2] = new Vector3 (0, 2, -9);
-        spawnPosList[3] = new Vector3 (3, 2, -5);
-        spawnPosList[4] = new Vector3 (3, 2, -7);
-        spawnPosList[5] = new Vector3 (3, 2, -9);
-        spawnPosList[6] = new Vector3 (6, 2, -5);
-        spawnPosList[7] = new Vector3 (6, 2, -7);
-        spawnPosList[8] = new Vector3 (6, 2, -9);
-
-
-    }
-    // Update is called once per frame
-    void SpawnInNewPos()
-    {
-        foreach(Vector3 item in spawnPosList)
-        {
-            Instantiate(spawnObject, item, Quaternion.identity);
-        }
-    }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !keyPressed)
         {
+            //invokerepeating("spawninnewpos", spawntime, spawntime);
             SpawnInNewPos();
+            keyPressed = true;
+            Debug.Log("pressed");
         }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            keyPressed = false;
+        }
+
+    }
+    void SpawnInNewPos()
+    {
+        int objectIndex = Random.Range(0, spawnPosList.Length);
+        GameObject stim = Instantiate(spawnObject, spawnPosList[objectIndex].position, Quaternion.identity);
+        GameObject.Destroy(stim, stimulusDuration);
+
     }
 }
+
+//OLD
+//{
+//    public GameObject spawnObject;
+//    public Vector3[] spawnPosList;
+//    private int spawnTotal;
+
+//    // Start is called before the first frame update
+//    private void Start()
+//    {
+//        spawnPosList = new Vector3[9];
+
+//        spawnPosList[0] = new Vector3 (0, 2, -5);
+//        spawnPosList[1] = new Vector3 (0, 2, -7);
+//        spawnPosList[2] = new Vector3 (0, 2, -9);
+//        spawnPosList[3] = new Vector3 (3, 2, -5);
+//        spawnPosList[4] = new Vector3 (3, 2, -7);
+//        spawnPosList[5] = new Vector3 (3, 2, -9);
+//        spawnPosList[6] = new Vector3 (6, 2, -5);
+//        spawnPosList[7] = new Vector3 (6, 2, -7);
+//        spawnPosList[8] = new Vector3 (6, 2, -9);
+
+
+//    }
+//    // Update is called once per frame
+//    void SpawnInNewPos()
+//    {
+//        foreach(Vector3 item in spawnPosList)
+//        {
+//            Instantiate(spawnObject, item, Quaternion.identity);
+//        }
+//    }
+
+//    private void Update()
+//    {
+//        if(Input.GetKeyDown(KeyCode.Space))
+//        {
+//            SpawnInNewPos();
+//        }
+//    }
+//}
 
 
 /// create a list of 9 vector3 spawn positions 
